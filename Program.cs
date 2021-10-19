e34b37e0850e0612825318358623276b888c4ed5
João Vitor Silva Maia <50407875+VitorSMaia@users.noreply.github.com>
2021-10-19 20:27:33 -0300

    Alterações Para Utiliza Apenas um Arquivo

--- Program.cs
+++ Program.cs
@@ -6,9 +6,9 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            short operacao = Menu();
-            (float NumeroI,float NumeroII) = InputDados(operacao);
-            Escolha(operacao,NumeroI,NumeroII);
+            short operador = Menu();
+            (float NumeroI,float NumeroII) = InputDados();
+            Resultado(operador,NumeroI,NumeroII);
         }
 
         static private short Menu()
@@ -22,36 +22,119 @@ namespace Calculator
             Console.WriteLine("0 - Sair");
             Console.WriteLine("<---------////--------->");
 
-            short escolha = short.Parse(Console.ReadLine());
-            return escolha;
-        }
-
-
-        static private (float NumeroI, float NumeroII) InputDados(short operacao)
-        {
+            short operacao = short.Parse(Console.ReadLine());
             if(operacao == 0)
             {
                 System.Environment.Exit(0);
             }
+            return operacao;
+        }
+
+
+        static private (float NumeroI, float NumeroII) InputDados()
+        {
             Console.WriteLine($"Qual é o Primeiro Número?");
             var numeroI = float.Parse(Console.ReadLine());
             Console.WriteLine($"Qual é o Segundo Número?");
             var numeroII = float.Parse(Console.ReadLine());
 
+            ValidaDenominador(numeroII);
+
             return (numeroI,numeroII);
         }
         
-        static private void Escolha(short escolha, float numeroI, float numeroII)
+        static private void Resultado(short operador, float numeroI, float numeroII)
         {
-            var Response = new Response();
-           switch (escolha)
+            var operacao = new Calculadora(numeroI, numeroII);
+            var mensagem = new Resposta();
+
+            if(operador == 1)
             {
-                case 1: Response.Response.Soma(numeroI,numeroII);                break;
-                case 2: Response.Response.Subtrair(numeroI,numeroII);            break;
-                case 3: Response.Response.Multiplicar(numeroI,numeroII);         break;
-                case 4: Response.Response.Dividir(numeroI,numeroII);             break;
-                default:  Response.Response.Soma(numeroI,numeroII);              break;
+                    float resultado = operacao.Somar();
+                    string msg = mensagem.Mensagem(operador, numeroI, numeroII, resultado);
+                    Console.WriteLine(msg);
             }
+            else if(operador == 2)
+            {
+                    float resultado = operacao.Subtrair();
+                    string msg = mensagem.Mensagem(operador, numeroI, numeroII, resultado);
+                    Console.WriteLine(msg);
+            }
+            else if(operador == 3)
+            {
+                    float resultado = operacao.Multiplicar();
+                    string msg = mensagem.Mensagem(operador, numeroI, numeroII, resultado);
+                    Console.WriteLine(msg);
+            }
+            else if(operador == 4)
+            {
+                    float resultado = operacao.Dividir();
+                    string msg = mensagem.Mensagem(operador, numeroI, numeroII, resultado);
+                    Console.WriteLine(msg);
+            }
+        }
+
+        private static void ValidaDenominador(float NumeroII)
+        {
+            if( NumeroII == 0 )
+            {
+                Console.WriteLine("O Segundo Denominador não pode ser 0 !");
+                System.Environment.Exit(0);
+            }
+        }
+    }
+    class Calculadora
+    {
+        public float  NumeroI{get;set;}
+        public float  NumeroII{get;set;}
+
+        public Calculadora(float numeroI, float numeroII)
+        {
+            NumeroI = numeroI;
+            NumeroII = numeroII;
+        }
+
+        public float Somar()
+        {
+            return NumeroI + NumeroII;
+        }
+        public float Subtrair()
+        {
+            return NumeroI - NumeroII;
+        }
+        public float Multiplicar()
+        {
+            return NumeroI * NumeroII;
+        }
+        public float Dividir()
+        {
+            return NumeroI / NumeroII;
+        }
+    }
+    class Resposta
+    {
+        public Resposta()
+        {
+
+        }
+        public string Mensagem(short operador, float numeroI, float numeroII,float resultado)
+        {
+            switch(operador)
+            {
+                case 1: 
+                    return $"O Resultado de {numeroI} + {numeroII} é igual {resultado}!";
+
+                case 2: 
+                    return $"O Resultado de {numeroI} - {numeroII} é igual {resultado}!";
+
+                case 3: 
+                    return $"O Resultado de {numeroI} * {numeroII} é igual {resultado}!";
+
+                case 4: 
+                    return $"O Resultado de {numeroI} / {numeroII} é igual {resultado}!";
+
+            }
+            return "0";
         }
     }
 }
--- Response/Response.cs
+++ Response/Response.cs
@@ -1,7 +1,5 @@
 using System;
 
-using Calculator.Calculate;
-
 namespace Calculator.Response
 {
     public class Response
